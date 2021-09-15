ddd68535e0c2bd0a3a677aeeab6d9610fca6be30
daemon2021cn <daemon2021#qq.com>
2021-09-15 03:27:38 -0700

    150921noon

--- 13-roman2integer.py
+++ 13-roman2integer.py
@@ -2,13 +2,20 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         dict_r = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
         out = 0
-        s_len = len(s)
+        s_last = dict_r[s[0]]
+        for i in s:
+            out = out+dict_r[i] if s_last>=dict_r[i] else out+dict_r[i]-2*s_last 
+            s_last = dict_r[i]
+        return out
+        #dict_r = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+        #out = 0
+        #s_len = len(s)
 
-        for i in range(s_len):
-            out += dict_r[s[i]]
-            out -= (2*dict_r[s[i-1]]) if dict_r[s[i-1]]<dict_r[s[i]] and (i-1)>=0 else 0
+        #for i in range(s_len):
+        #    out += dict_r[s[i]]
+        #    out -= (2*dict_r[s[i-1]]) if dict_r[s[i-1]]<dict_r[s[i]] and (i-1)>=0 else 0
 
-        return out     
+        #return out     
 
 classNew = Solution()
 s = input()
