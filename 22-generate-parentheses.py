d8dc20cb976934102a7d9b3fe71218f5596081fc
daemon2021cn <daemon2021#qq.com>
2021-09-17 12:13:21 -0700

    update170921

--- 22-generate-parentheses.py
+++ 22-generate-parentheses.py
@@ -1,15 +1,28 @@
 from typing import List
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        def generateParen(cur_p, left, right, res=[]):
-            if left:
-                generateParen(cur_p+'(', left-1, right)
-            if right>left:
-                generateParen(cur_p+')', left, right-1)
-            if right == 0:
-                res += cur_p, #',' will create a new list or tuple
-            return res
-        return generateParen('', n, n)
+        p_list = [('(', 1, 0)]
+        res = []
+
+        while p_list:
+            p, l, r = p_list.pop()
+            if l<r or l>n or r>n:
+                continue
+            if l==n and r==n:
+                res.append(p)
+            p_list.append([p+'(', l+1, r])
+            p_list.append([p+')', l, r+1])
+        return res
+
+        # def generateParen(cur_p, left, right, res=[]):
+        #     if left:
+        #         generateParen(cur_p+'(', left-1, right)
+        #     if right>left:
+        #         generateParen(cur_p+')', left, right-1)
+        #     if right == 0:
+        #         res += cur_p, #',' will create a new list or tuple
+        #     return res
+        # return generateParen('', n, n)
 
         # out = ["()"]
 
